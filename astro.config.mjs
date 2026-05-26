12d3c325d63a427158d8c1e0f7917847b65abb46
nikuuun <takethislove@gmail.com>
2026-05-26 19:35:53 -0400

    Instalar Tailwind CSS v4 y agregar logo.jpg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' },
   },
+
   site: 'https://accelerazione.com',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
